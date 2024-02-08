c215ee3c6bed337e36496551f7230e4171e35a20
Victor <victor.garcia@singularly.com>
2024-02-08 09:50:33 +0100

    add nt track

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script 
-          src="https://unpkg.com/@neuraltrust/nt.js@1.0.45" 
+          src="https://unpkg.com/@neuraltrust/nt.js@1.0.46" 
           data-token="test" 
           data-llm-endpoint="ai-chatbot-o53syvjs8-vgmartinez.vercel.app"
           data-llm-path="/api/chat"
