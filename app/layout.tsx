c02a1d83a94e0a771139d51887b24b7bd7fa9e0d
Victor <victor.garcia@singularly.com>
2024-02-08 09:13:55 +0100

    add nt track

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from 'react-hot-toast'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-
+import Script from 'next/script'
 import '@/app/globals.css'
 import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
@@ -37,13 +37,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script 
+        <Script 
         src="https://unpkg.com/@neuraltrust/nt.js@1.0.41" 
         data-token="test" 
         data-llm-endpoint="us-central1-neuraltrust-app-dev.cloudfunctions.net/neuraltrust-demo-api/v1/chat"
         data-res-prompt="message"
         data-req-prompt="prompt"
-        ></script>
+        ></Script>
       </head>
       <body
         className={cn(
