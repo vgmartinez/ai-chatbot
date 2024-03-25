0678ea26f63b87c73d32b8b6506c4bda4651563b
Victor <victor.garcia@singularly.com>
2024-03-25 09:57:17 +0100

    increase version

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -17,48 +17,52 @@ export async function POST(req: Request) {
   const userId = (await auth())?.user.id
   //const userId = (await auth())?.user.id || 'anonymous'
 
-
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
+  try {
+    if (!userId) {
+      return new Response('Unauthorized', {
+        status: 401
+      })
+    }
+  
+    if (previewToken) {
+      openai.apiKey = previewToken
+    }
+  
+    const completions = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages,
+      temperature: 0.7,
+      stream: false
     })
+    const completion = completions.choices[0]
+    const title = completion.message.content?.substring(0, 100)
+    const id = nanoid()
+    const createdAt = Date.now()
+    const path = `/chat/${id}`
+    const payload = {
+      id,
+      title,
+      userId,
+      createdAt,
+      path,
+      messages: [
+        ...messages,
+        {
+          content: completion.message.content,
+          role: 'assistant'
+        }
+      ]
+    }
+    
+    await kv.hmset(`chat:${id}`, payload)
+    await kv.zadd(`user:chat:${userId}`, {
+      score: createdAt,
+      member: `chat:${id}`
+    })
+    return Response.json({...completion.message})
+  } catch(e) {
+    console.log(e)
   }
 
-  if (previewToken) {
-    openai.apiKey = previewToken
-  }
-
-  const completions = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: false
-  })
-
-  const completion = completions.choices[0]
-  const title = completion.message.content?.substring(0, 100)
-  const id = nanoid()
-  const createdAt = Date.now()
-  const path = `/chat/${id}`
-  const payload = {
-    id,
-    title,
-    userId,
-    createdAt,
-    path,
-    messages: [
-      ...messages,
-      {
-        content: completion.message.content,
-        role: 'assistant'
-      }
-    ]
-  }
-  await kv.hmset(`chat:${id}`, payload)
-  await kv.zadd(`user:chat:${userId}`, {
-    score: createdAt,
-    member: `chat:${id}`
-  })
-
-  return Response.json({...completion.message})
+  
 }
